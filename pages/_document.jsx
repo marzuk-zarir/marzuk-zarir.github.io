1eee4e782a1ea4ec498686311df158786f13da75
marzuk-zarir <business.marzukzarir@gmail.com>
2022-11-03 23:58:34 +0600

    feat: add support for favicon and repo secrets

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
                     rel="stylesheet"
